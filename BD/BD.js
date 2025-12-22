8c2e7ee17c2e46d86fb770e30bc2219d3f46b815
Frideraid935 <jossuavelazquez24@gmail.com>
2025-12-21 20:54:43 -0600

    BD corregido

--- BD/BD.js
+++ BD/BD.js
@@ -3,3 +3,5 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
+
+console.log("MYSQL_URL:", process.env.MYSQL_URL ? "OK" : "UNDEFINED");
