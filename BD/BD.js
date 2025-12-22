13a18a45b05feae270077be4a87a2405faaf7714
Frideraid935 <jossuavelazquez24@gmail.com>
2025-12-21 20:51:08 -0600

    BD corregido

--- BD/BD.js
+++ BD/BD.js
@@ -1,14 +1,5 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQLPORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
-console.log("MYSQLHOST:", process.env.MYSQLHOST);
