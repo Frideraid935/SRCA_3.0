89d53f2a10ca0736f6a537b7f2f199ea7d597f59
Frideraid935 <jossuavelazquez24@gmail.com>
2025-12-29 22:09:39 -0600

    modulo gestion de alumnos

--- publico/JS/alumnos-menu.js
+++ publico/JS/alumnos-menu.js
@@ -5,20 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
 
   menuItems.forEach(item => {
     item.addEventListener("click", () => {
+
       // Quitar activo a todos
       menuItems.forEach(i => i.classList.remove("active"));
 
       // Activar el actual
       item.classList.add("active");
 
-      // Cargar el HTML correspondiente en el iframe
-      const form = item.getAttribute("data-form");
-      iframe.src = `${form}.html`;
+      // Cargar HTML en iframe (RUTA ABSOLUTA)
+      const form = item.dataset.form;
+      iframe.src = `/Modulo-Alumno-Profesores-Admin/${form}.html`;
     });
   });
 
   // Botón menú principal
-  btnInicio.addEventListener("click", () => {
-    window.location.href = "/"; // o a tu dashboard principal
-  });
+  if (btnInicio) {
+    btnInicio.addEventListener("click", () => {
+      window.location.href = "/menu_principal_alumnos.html";
+    });
+  }
 });
